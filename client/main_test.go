fceafadfad904db287a0353e3189656a4d4bb32d
Memo Chou <memochou1993@gmail.com>
2021-01-11 22:17:28 +0800

    Update client test

--- client/main_test.go
+++ client/main_test.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"github.com/memochou1993/worker-service/client/handler"
 	gw "github.com/memochou1993/worker-service/gen"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 	"testing"
 	"time"
 )
@@ -14,12 +12,7 @@ func TestGetWorker(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	_, err := handler.Client.GetWorker(ctx, &gw.GetWorkerRequest{})
-	s, ok := status.FromError(err)
-	if !ok {
-		t.Fatal(err.Error())
-	}
-	if s.Code() == codes.NotFound {
+	if _, err := handler.Client.GetWorker(ctx, &gw.GetWorkerRequest{}); err != nil {
 		t.Fatal(err.Error())
 	}
 }
@@ -28,7 +21,29 @@ func TestPutWorker(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	if _, err := handler.Client.PutWorker(ctx, &gw.PutWorkerRequest{Number: 1}); err != nil {
+	if _, err := handler.Client.PutWorker(ctx, &gw.PutWorkerRequest{Number: 100}); err != nil {
+		t.Fatal(err.Error())
+	}
+}
+
+func TestListWorkers(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	if _, err := handler.Client.ListWorkers(ctx, &gw.ListWorkersRequest{}); err != nil {
+		t.Fatal(err.Error())
+	}
+}
+
+func TestShowWorkers(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	if _, err := handler.Client.PutWorker(ctx, &gw.PutWorkerRequest{Number: 100}); err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if _, err := handler.Client.ShowWorker(ctx, &gw.ShowWorkerRequest{Number: 100}); err != nil {
 		t.Fatal(err.Error())
 	}
 }
