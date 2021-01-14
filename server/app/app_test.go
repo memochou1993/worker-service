798bc24dc00e52b3b33353d4949ea6d215bb9a39
Memo Chou <memochou1993@gmail.com>
2021-01-14 15:43:44 +0800

    Use major and minor channels

--- server/app/app_test.go
+++ server/app/app_test.go
@@ -11,8 +11,9 @@ func TestEnqueue(t *testing.T) {
 	number := 50
 	service := NewService(options.Service().SetMaxWorkers(number))
 
-	for i := 0; i < number; i++ {
-		<-service.Workers
+	for i := 0; i < number/2; i++ {
+		<-service.Major
+		<-service.Minor
 	}
 
 	wg := sync.WaitGroup{}
@@ -25,7 +26,10 @@ func TestEnqueue(t *testing.T) {
 	}
 	wg.Wait()
 
-	if len(service.Workers) != number {
+	if len(service.Major) != number/2 {
+		t.Fail()
+	}
+	if len(service.Minor) != number/2 {
 		t.Fail()
 	}
 }
@@ -46,7 +50,10 @@ func TestDequeue(t *testing.T) {
 	}
 	wg.Wait()
 
-	if len(service.Workers) != 0 {
+	if len(service.Major) > 0 {
+		t.Fail()
+	}
+	if len(service.Minor) > 0 {
 		t.Fail()
 	}
 }
